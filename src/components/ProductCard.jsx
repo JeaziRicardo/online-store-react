bd5134b110fe3f0216ca1b0e42127899149ea119
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-04-26 20:47:38 -0300

    Cria o componente ProductCard para gerar os produtos

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class ProductCard extends Component {
+  render() {
+    const { data } = this.props;
+    const { results } = data;
+    return (
+      <section>
+        {results.map(({ title, price, thumbnail, id }) => (
+          <div key={ id }>
+            <h3>{ title }</h3>
+            <img src={ thumbnail } alt={ title } />
+            <p>{ price }</p>
+          </div>
+        ))}
+      </section>
+    );
+  }
+}
+
+ProductCard.propTypes = {
+  data: PropTypes.shape().isRequired,
+};
+
+export default ProductCard;
