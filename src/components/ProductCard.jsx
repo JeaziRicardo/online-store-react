c4ee364df2f0381f8f209a648d97619b45455783
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-04-26 23:28:56 -0300

    Corrige a maneira de exibir o preço do produto

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -11,7 +11,7 @@ class ProductCard extends Component {
           <div key={ id } data-testid="product">
             <h3>{ title }</h3>
             <img src={ thumbnail } alt={ title } />
-            <p>{ price }</p>
+            <p>{ `R$ ${price}` }</p>
           </div>
         ))}
       </section>
