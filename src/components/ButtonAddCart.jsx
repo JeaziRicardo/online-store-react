55065f4565294dfac47d230cd3d6b1f4239d2da3
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-04-27 19:32:28 -0300

    Cria o componente ButtonAddCart

---
+++ src/components/ButtonAddCart.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class ButtonAddCart extends Component {
+  render() {
+    return (
+      <div>
+        <button type="button">Adicionar ao carrinho</button>
+      </div>
+    );
+  }
+}
