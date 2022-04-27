27dc905c58116b1754590f5db271edc66378fb08
Thiago Dias <thiaguinhodias.15@gmail.com>
2022-04-27 18:42:41 -0300

    adicionando validacao com propTypes

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,9 +1,36 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { getProductDetails } from '../services/api';
 
 export default class ProductDetails extends Component {
+  constructor() {
+    super();
+    this.state = {
+      productDetails: '',
+    };
+  }
+
+  async componentDidMount() {
+    const { match: { params: { id } } } = this.props;
+
+    const productDetails = await getProductDetails(id);
+
+    this.setState({
+      productDetails,
+    });
+  }
+
   render() {
+    const { productDetails: { title } } = this.state;
+
     return (
-      <div>ProductDetails</div>
+      <div>
+        <h1 data-testid="product-detail-name">{ title }</h1>
+      </div>
     );
   }
 }
+
+ProductDetails.propTypes = {
+  match: PropTypes.object,
+}.isRequired;
