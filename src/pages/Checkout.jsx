11466fedf29211a16710b75b3c2b668b34322913
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-05-02 21:41:06 -0300

    Cria a página Checkout

---
+++ src/pages/Checkout.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Checkout extends Component {
+  render() {
+    return (
+      <h1>Revise e confirme sua compra</h1>
+    );
+  }
+}
+
+export default Checkout;
